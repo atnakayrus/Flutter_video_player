bb4f54862233e062a50d554145fa6c9bab70835e
dj <divyajyotibose@gmail.com>
2023-03-28 01:18:18 +0530

    bottom navbar
    pages change now

--- lib/screens/BaseScreen.dart
+++ lib/screens/BaseScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/screens/FileScreen.dart';
+import 'package:video_player/screens/WebScreen.dart';
+
+import 'FavScreen.dart';
 
 class BaseScreen extends StatefulWidget {
   const BaseScreen({super.key});
@@ -8,12 +12,28 @@ class BaseScreen extends StatefulWidget {
 }
 
 class _BaseScreenState extends State<BaseScreen> {
+  int _selectedIndex=0;
+  void SelectedItem(int index){
+    setState(() {
+      _selectedIndex=index;
+    });
+  }
+  Widget currentScreen(){
+    if(_selectedIndex==1){
+      return FileScreen();
+    }
+    else if(_selectedIndex==2){
+      return WebScreen();
+    }
+    return FavScreen();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('hello'),
       ),
+      body: currentScreen(),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
@@ -29,6 +49,8 @@ class _BaseScreenState extends State<BaseScreen> {
             label: 'web',
           ),
         ],
+        currentIndex: _selectedIndex,
+        onTap: SelectedItem,
       ),
     );
   }
