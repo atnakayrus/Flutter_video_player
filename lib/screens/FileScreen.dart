bbc58495fa39a7427329c0c1f2566c436c48c8a3
dj <divyajyotibose@gmail.com>
2023-03-28 20:53:22 +0530

    file explorer

--- lib/screens/FileScreen.dart
+++ lib/screens/FileScreen.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
-
+import "package:file_manager/file_manager.dart";
 class FileScreen extends StatefulWidget {
   const FileScreen({super.key});
 
@@ -8,11 +10,37 @@ class FileScreen extends StatefulWidget {
 }
 
 class _FileScreenState extends State<FileScreen> {
+  FileManagerController controller=FileManagerController();
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.yellow,
-      child: const Text('hello'),
+      child: FileManager(
+        controller: controller,
+        builder: (context, snapshot) {
+          final List<FileSystemEntity> entities = snapshot;
+          return ListView.builder(
+            itemCount: entities.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  leading: FileManager.isFile(entities[index])
+                      ? Icon(Icons.feed_outlined)
+                      : Icon(Icons.folder),
+                  title: Text(FileManager.basename(entities[index])),
+                  onTap: () {
+                    if (FileManager.isDirectory(entities[index])) {
+                      controller.openDirectory(entities[index]);   // open directory
+                    } else {
+                      // Perform file-related tasks.
+                    }
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
     ;
   }
